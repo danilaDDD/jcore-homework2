102da6136431a1ca854f5a3b976192cb89b81821
danila nikitin <danila.n2015@yandex.ru>
2024-07-16 18:27:06 +0300

    add counting sort

--- src/ru/gb/examples/Main.java
+++ src/ru/gb/examples/Main.java
@@ -1,15 +1,53 @@
 package ru.gb.examples;
 
+// counting sort
 //1)Написать метод, возвращающий количество чётных элементов массива. countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
 //2)Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами переданного не пустого массива.
 //3)Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с нулевым значением.
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 9, 1, 4, 1, 2, 0, 0, 9};
+        System.out.println("sorted array: " + Arrays.toString(countingSort(array)));
         System.out.println("events count: " + countEvents(array));
         System.out.println("different between max and min: " + maxMinDifferent(array));
         System.out.println("existing 2 neighboring zero items: " + neighboringZeroExist(array));
+    }
+
+    private static int[] countingSort(int[] array){
+        if(array.length <= 1)
+            return Arrays.copyOf(array, array.length);
+
+        int min = array[0], max = array[0];
+        for (int e: array) {
+            if(e < min)
+                min = e;
+            if(e > max)
+                max = e;
+        }
+
+        // calc counters
+        int uniqueNumbers = max + 1 - min;
+        int[] countersArray = new int[uniqueNumbers];
+        for(int e: array){
+            int counterIndex = e - min;
+            countersArray[counterIndex] += 1;
+        }
+
+        int[] sortedArray = new int[array.length];
+        int index = 0;
+        for (int i = 0; i < countersArray.length; i++) {
+            int number = min + i;
+            int counter = countersArray[number];
+            for (int j = 0; j < counter; j++) {
+                sortedArray[index ++] = number;
+            }
+        }
+
+
+        return sortedArray;
     }
 
     private static boolean neighboringZeroExist(int[] array) {
